8f3c63dba60f8d6cf689cdd220ae637d3092c891
YajanaRao <yajananrao@gmail.com>
2019-07-12 18:26:58 +0530

    Added new api

--- android/src/main/java/com/rnmediametadataretriever/RNMediaMetadataRetrieverModule.java
+++ android/src/main/java/com/rnmediametadataretriever/RNMediaMetadataRetrieverModule.java
@@ -3,42 +3,71 @@ package com.rnmediametadataretriever;
 import android.media.MediaMetadataRetriever;
 import android.widget.Toast;
 
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
 
-import java.util.Map;
 import java.util.HashMap;
 
-public class RNMediaMetadataRetrieverModule extends ReactContextBaseJavaModule {
 
-  private static final int METADATA_KEY_ALBUM = 1;
-  private static final int METADATA_KEY_ALBUMARTIST = 13;
-  private static final int METADATA_KEY_ARTIST = 2;
-  private static final int METADATA_KEY_AUTHOR = 3;
-  private static final int METADATA_KEY_DURATION = 9;
-  private static final int METADATA_KEY_GENRE = 6;
-  private static final int METADATA_KEY_NUM_TRACKS = 10;
-  private static final int METADATA_KEY_TITLE = 7;
+
+public class RNMediaMetadataRetrieverModule extends ReactContextBaseJavaModule {
   
+  private static final String NOT_FOUND_ERROR = "NOT_FOUND_ERROR";
+
   @Override
   public String getName() {
     return "RNMediaMetadataRetriever";
   }
 
 
+  @ReactMethod
+  public void getMetadata(String uri, Promise promise) {
+     WritableMap map = Arguments.createMap();
+     MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+    try{
+      mmr.setDataSource(uri, new HashMap<String, String>());
+      String songGenre = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_GENRE);
+      String album =  mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
+      String duration = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+      String artist = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+      String title = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+
+      map.putString("genre"  ,songGenre);
+      map.putString("album",album);
+      map.putString("duration",duration);
+      map.putString("artist", artist);
+      map.putString("title", title);
+      promise.resolve(map);
+    } catch(RuntimeException exp){
+       promise.reject(NOT_FOUND_ERROR,exp);
+    } finally {
+      mmr.release();
+    }
+  }
 
   @ReactMethod
-  public void show(String uri) {
+  public void getPicture(String uri, Promise promise){
+    WritableMap map = Arguments.createMap();
+    MediaMetadataRetriever mmr = new MediaMetadataRetriever();
     try{
-      MediaMetadataRetriever mmr = new MediaMetadataRetriever();
-      mmr.setDataSource(uri);
-      String songGenre = mmr.extractMetadata(mmr.METADATA_KEY_GENRE);
-      Toast.makeText(getReactApplicationContext(), songGenre, Toast.LENGTH_LONG).show();
+      byte[] data = mmr.getEmbeddedPicture();
+      if(data != null){
+        String artcover = new String();
+        map.putString("artcover", artcover);
+      } else{
+        map.putString("artcover", null);
+      }
+      promise.resolve(map);
     } catch(RuntimeException exp){
-       Toast.makeText(getReactApplicationContext(), "error", Toast.LENGTH_LONG).show();
+      promise.reject(NOT_FOUND_ERROR,exp);
+    } finally {
+      mmr.release();
     }
   }
 
